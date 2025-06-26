c32722bc16f71e38b3e07638ff7c0450b1ff4b1b
Mohid Warsi <mohid.warsi@conurets.com>
2025-06-26 18:47:17 +0500

    fixed responsive

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ import Spline from "@splinetool/react-spline";
 const Hero = () => {
 	return (
 		<section className="relative w-full h-screen mx-auto">
-			<div className="paddingX absolute inset-0 xs:top-[220px] md:top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
+			<div className="paddingX absolute inset-0 top-[220px] md:top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
 				<div className="flex flex-col justify-center items-center mt-5">
 					<div className="w-5 h-5 rounded-full bg-[#915EFF] " />
 					<div className="w-1 sm:h-80 h-40 violet-gradient" />
@@ -33,7 +33,7 @@ const Hero = () => {
 				/>
 			</div> */}
 			{/* Replace Image with Spline 3D */}
-			<div className="hidden xl:block">
+			<div className="opacity-0 xl:opacity-100">
 				<div className="w-full flex justify-center items-center pt-[40vh] md:pt-32 relative">
 					<Spline scene="https://prod.spline.design/6VfGj6YlCKk4eouZ/scene.splinecode" />
 					<div className="absolute inset-0 z-10 w-full h-full" />
